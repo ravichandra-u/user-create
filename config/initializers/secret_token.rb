9b2987eabcefeb12aeb64d66c6a0a538eb2a0e9f
Coppernine4 <coppernine4@coppernine4-desktop.(none)>
2013-03-23 13:01:57 +0530

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Simpleform::Application.config.secret_token = '43ca7bfb864bf19b3ce4067cee489f4b5dd54b4037460a9658fac70fd64d3e48fc2069a8dfe61e35c16973e90bb6cb04da36d6b8c0609ad3b1ed2b9f6159f2bf'
